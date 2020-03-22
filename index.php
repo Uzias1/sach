edb0d2230f7ab647b872e72465dc827c70f1f68c
Uzias1 <38708130+Uzias1@users.noreply.github.com>
2020-03-22 00:41:31 -0600

    siu

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 		<link rel="stylesheet" type="text/css" href="css/estilo.css">
         <script type="text/javascript" src="js/funcion.js"></script>
     </head>
-    <body>
+    <body onload="funcionxd();">
     <div class="contenedor">
         <div class="derecha">
             <h3 class="tituloPuntuaciones">MEJORES PUNTUACIONES</h3>
@@ -13,14 +13,16 @@
         </div>
         <div class="juego">
             <canvas id="canvas" width="515" height="327">TU NAVEGADOR NO SOPORTA HTML5!</canvas>
-            <button id="botonan" onclick="comenzar()">Comenzar</button>
+            <!--
+                <button id="botonan" onclick="comenzar()">Comenzar</button>
             <button id="boton1" onclick="contadorclics()">Clic on me</button>
-            <!--<a href="javascript:clickLink();">Click aquí</a> -->
+            !--<a href="javascript:clickLink();">Click aquí</a> 
         	<p>Clicks <span id="clicks">0</span></p>
         	<button onclick="Reset()">Reset</button>
-        	<!--<button onclick="aleatorio()">Holaaaa</button>-->
+        	!--<button onclick="aleatorio()">Holaaaa</button>
         	<p><span id="cuenta_regresiva">10</span></p>
         	<p><span id="hey">10</span></p>
+            -->
         </div>
     </div>
     </body>
